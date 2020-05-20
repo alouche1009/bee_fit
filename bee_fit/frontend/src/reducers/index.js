425529e3371ecad33a35f90cb401a5750a58bc1a
alouche1009 <aurore.louche@epitech.eu>
2020-05-20 15:50:35 +0200

    merged ok

--- bee_fit/frontend/src/reducers/index.js
+++ bee_fit/frontend/src/reducers/index.js
@@ -2,19 +2,12 @@ import { combineReducers } from 'redux';
 import infos from './infos.js';
 import errors from './errors';
 import messages from './messages'
-<<<<<<< HEAD
 import auth from './auth'; // added
-=======
->>>>>>> mergedprofillog
 
 export default combineReducers({
     infos,
     errors,
-<<<<<<< HEAD
     messages,
     auth // added
-=======
-    messages
->>>>>>> mergedprofillog
 
 });
